18e6dd468f83c02cf1d25282368dc96d5397240a
matfij <mateusz.michal.fijak@gmail.com>
2021-11-07 19:05:36 +0100

    fix frontend network

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,12 @@
+import { NestApplicationOptions } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: '*' });
+  app.setGlobalPrefix('api/v1');
+
   await app.listen(process.env.PORT);
 }
 bootstrap();
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,22 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  title = 'data loading ...';
+  title = 'status loading ...';
+  baseUrl = environment.apiUrl;
 
   constructor(
     private http: HttpClient
   ) {}
 
-
   ngOnInit() {
-    this.http.get('/start')
+    this.http.get<Status>(this.baseUrl + '/start')
       .subscribe(
-        (response) => {
-          this.title = response.toString();
+        (response: Status) => {
+          this.title = response['status'].toString();
         },
         (error) => {
           console.log(error);
         }
       );
   }
+}
+
+interface Status {
+  status: string;
 }
