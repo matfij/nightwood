34d04bde648f8435fec4ec8f6ed5baf40320ced5
matfij <mateusz.michal.fijak@gmail.com>
2022-06-18 11:49:34 +0200

    backend - add new booster definitions

--- backend/src/api/items/alchemy/model/data/booster-recipe.ts
+++ backend/src/api/items/alchemy/model/data/booster-recipe.ts
@@ -1,7 +1,7 @@
 import { BUBULAE_STEAK, IHON_BERRY, RELIQUM_EGG, RORIS_LEAVES, SPIRAL_NUT } from "src/api/items/item/model/data/food";
 import { ARTICHOKE, SOVAGA_LEAVES } from "src/api/items/item/model/data/ingredients";
 import { BoosterRecipeDto } from "../dto/booster-recipe.dto";
-import { BOOSTER_AGILITY, BOOSTER_ATTACK, BOOSTER_DEFENSE, BOOSTER_WISDOM } from "./boosters";
+import { BOOSTER_AGILITY, BOOSTER_ATTACK, BOOSTER_DEFENSE, BOOSTER_FULL, BOOSTER_SPECIAL, BOOSTER_WISDOM } from "./boosters";
 
 export const RECIPE_BOOSTER_ATTACK: BoosterRecipeDto = {
     uid: 'recipe-b-1',
@@ -47,9 +47,39 @@ export const RECIPE_BOOSTER_WISDOM: BoosterRecipeDto = {
     ],
 };
 
+export const RECIPE_BOOSTER_FULL: BoosterRecipeDto = {
+    uid: 'recipe-b-5',
+    product: { ...BOOSTER_FULL, quantity: 1 },
+    ingredients: [
+        { ...SOVAGA_LEAVES, quantity: 12 },
+        { ...BUBULAE_STEAK, quantity: 6 },
+        { ...RELIQUM_EGG, quantity: 6 },
+        { ...IHON_BERRY, quantity: 6 },
+        { ...SPIRAL_NUT, quantity: 6 },
+        { ...RORIS_LEAVES, quantity: 6 },
+        { ...ARTICHOKE, quantity: 2 },
+    ],
+};
+
+export const RECIPE_BOOSTER_SPECIAL: BoosterRecipeDto = {
+    uid: 'recipe-b-6',
+    product: { ...BOOSTER_SPECIAL, quantity: 1 },
+    ingredients: [
+        { ...SOVAGA_LEAVES, quantity: 12 },
+        { ...BUBULAE_STEAK, quantity: 6 },
+        { ...RELIQUM_EGG, quantity: 6 },
+        { ...IHON_BERRY, quantity: 6 },
+        { ...SPIRAL_NUT, quantity: 6 },
+        { ...RORIS_LEAVES, quantity: 6 },
+        { ...ARTICHOKE, quantity: 2 },
+    ],
+};
+
 export const BOOSTER_RECIPES: BoosterRecipeDto[] = [
     RECIPE_BOOSTER_ATTACK,
     RECIPE_BOOSTER_DEFENCE,
     RECIPE_BOOSTER_AGILITY,
     RECIPE_BOOSTER_WISDOM,
+    RECIPE_BOOSTER_FULL,
+    RECIPE_BOOSTER_SPECIAL,
 ];
--- backend/src/api/items/alchemy/model/data/boosters.ts
+++ backend/src/api/items/alchemy/model/data/boosters.ts
@@ -50,9 +50,39 @@ export const BOOSTER_WISDOM: ItemDto = {
     }
 };
 
+export const BOOSTER_FULL: ItemDto = {
+    uid: 'item-b-5',
+    name: 'Pure Strength',
+    level: 30,
+    type: ItemType.Booster,
+    rarity: ItemRarity.Scarce,
+    statistics: {
+        physicalAttackBoost: 0.15,
+        magicalAttackBoost: 0.15,
+        healthBoost: 0.15,
+        speedBoost: 0.15,
+    }
+};
+
+export const BOOSTER_SPECIAL: ItemDto = {
+    uid: 'item-b-6',
+    name: 'Helping Hand',
+    level: 30,
+    type: ItemType.Booster,
+    rarity: ItemRarity.Scarce,
+    statistics: {
+        criticalChanceBoost: 0.3,
+        manaBoost: 0.3,
+        armorBoost: 0.3,
+        dodgeBoost: 0.3,
+    }
+}
+
 export const BOOSTERS = [
     BOOSTER_ATTACK,
     BOOSTER_DEFENSE,
     BOOSTER_AGILITY,
     BOOSTER_WISDOM,
+    BOOSTER_FULL,
+    BOOSTER_SPECIAL,
 ];
