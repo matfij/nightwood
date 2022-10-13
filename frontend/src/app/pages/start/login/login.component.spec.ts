45f931d2eb4f1ba58f1163de0cbe4b335b52ed7e
matfij <mateusz.michal.fijak@gmail.com>
2022-10-13 22:14:02 +0200

    frontend - add login component render tests

--- frontend/src/app/pages/start/login/login.component.spec.ts
+++ frontend/src/app/pages/start/login/login.component.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
 import { RouterTestingModule } from "@angular/router/testing";
 import { TranslateModule } from "@ngx-translate/core";
 import { ToastrModule } from "ngx-toastr";
@@ -23,9 +24,40 @@ describe('LoginComponent', () => {
     httpTestingController = TestBed.inject(HttpTestingController);
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render app brand name, hint and image', () => {
+    const nameElement = fixture.debugElement.query(By.css('[data-testid="app-title"]')).nativeElement;
+    const hintElement = fixture.debugElement.query(By.css('[data-testid="app-hint"]')).nativeElement;
+    const brandImageElement = fixture.debugElement.query(By.css('[data-testid="app-brand-image"]')).nativeElement;
+
+    expect(nameElement.textContent).toBe('common.appName');
+    expect(hintElement.textContent).toBe('common.appHint');
+    expect(brandImageElement.src).toContain('/assets/img/app/logo.png');
+  });
+
+  it('should render login form', () => {
+    const nicknameLabelElement = fixture.debugElement.query(By.css('[data-testid="app-nickname-label"]')).nativeElement;
+    const nicknameInutElement = fixture.debugElement.query(By.css('[data-testid="app-nickname-input"]')).nativeElement;
+    const passwordLabelElement = fixture.debugElement.query(By.css('[data-testid="app-password-label"]')).nativeElement;
+    const passwordInputElement = fixture.debugElement.query(By.css('[data-testid="app-password-input"]')).nativeElement;
+    const loginButtonElement = fixture.debugElement.query(By.css('[data-testid="app-login-button"]')).nativeElement;
+    const registerButtonElement = fixture.debugElement.query(By.css('[data-testid="app-register-button"]')).nativeElement;
+
+    expect(nicknameLabelElement.textContent).toBe('start.nickname');
+    expect(nicknameInutElement).toBeTruthy();
+    expect(passwordLabelElement.textContent).toBe('start.password');
+    expect(passwordInputElement).toBeTruthy();
+    expect(loginButtonElement.textContent).toBe(' start.login ');
+    expect(registerButtonElement.textContent).toBe(' start.register ');
+  });
+
+  it('should login successfully', () => {
+
   });
 });
