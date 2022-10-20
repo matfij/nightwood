bd74603cdccb97b3f9aedf1629670503426961a3
matfij <mateusz.michal.fijak@gmail.com>
2022-10-20 21:16:54 +0200

    backend - add email service tests

---
+++ backend/src/common/services/email.service.spec.ts
@@ -0,0 +1,58 @@
+import { MailerModule, MailerService } from "@nestjs-modules/mailer";
+import { Test } from "@nestjs/testing";
+import { EmailReplaceToken, EmailType } from "../definitions/emails";
+import { EmailService } from "./email.service";
+import { ErrorService } from "./error.service";
+
+describe('EmailService', () => {
+    let emailService: EmailService;
+    let mailerService: MailerService;
+    let errorService: ErrorService;
+
+    beforeEach(async () => {
+        const moduleRef = await Test.createTestingModule({
+            imports: [
+                MailerModule.forRoot({
+                    transport: {
+                        host: '0.0.0.0',
+                        port: 0,
+                    }
+                })
+            ],
+            providers: [
+                EmailService,
+                ErrorService,
+            ]
+        }).compile();
+
+        emailService = moduleRef.get<EmailService>(EmailService);
+        mailerService = moduleRef.get<MailerService>(MailerService);
+        errorService = moduleRef.get<ErrorService>(ErrorService);
+    });
+
+    it('should create the service', () => {
+        expect(emailService).toBeTruthy();
+    });
+
+    it('should successfully send email', () => {
+        const receiver = 'test@mail.com';
+        const type = EmailType.Activation;
+        const tokens: EmailReplaceToken[] = [
+            { token: '$email', value: receiver }
+        ];
+
+        const sendEmailSpy = jest.spyOn(mailerService, 'sendMail').mockImplementation(() => 
+            new Promise(() => {})
+        );
+
+        emailService.sendEmail(receiver, type, tokens);
+
+        expect(sendEmailSpy).toBeCalledWith(
+            expect.objectContaining({
+                to: receiver,
+                from: 'Nightwood@re.nightwood.dev',
+                subject: 'Nightwood - activate your account'
+            })
+        );
+    });
+});
