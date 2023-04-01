8f5b4d0d686a14bdf7d72634cf52179e2cf99843
matfij <mateusz.michal.fijak@gmail.com>
2023-04-01 10:57:34 +0200

    implement first anniversary event

--- backend/src/api/dragons/dragon-action/data/expedition-guardians-event.ts
+++ backend/src/api/dragons/dragon-action/data/expedition-guardians-event.ts
@@ -1,5 +1,27 @@
 import { ExpeditionGuardianDto } from "../model/dto/expedition-guardian.dto";
 
+export const GUARDIAN_ANNIVERSARY: ExpeditionGuardianDto = {
+    uid: 'guardian-e0',
+    name: 'Slyphora, Dragonoid',
+    level: 25,
+    strength: 20,
+    dexterity: 10,
+    endurance: 10,
+    will: 5,
+    luck: 0,
+    skills: {
+        deepWounds: 10,
+        lifeLink: 1,
+        rage: 10,
+        block: 10,
+        greatVigor: 25,
+        inferialBlessing: 10,
+        fireBolt: 10,
+    },
+    runes: [],
+    boosterUid: null,
+}
+
 export const GUARDIAN_EVENT_1: ExpeditionGuardianDto = {
     uid: 'guardian-e1',
     name: 'Santaworks',
--- backend/src/api/dragons/dragon-action/data/expeditions-event.ts
+++ backend/src/api/dragons/dragon-action/data/expeditions-event.ts
@@ -1,7 +1,33 @@
 import { SHARD_BLANK, SHARD_ATTACK, SHARD_DEFENCE, SHARD_AGILITY, SHARD_WISDOM, SHARD_UNITY, SCORIA_ORE, AZURE_PEBBLE } from "src/api/items/item/data/ingredients";
 import { EXPEDITION_TIME_SCALE } from "src/configuration/backend.config";
 import { ExpeditionDto } from "../model/dto/expedition.dto";
-import { GUARDIAN_EVENT_1 } from "./expedition-guardians-event";
+import { GUARDIAN_ANNIVERSARY, GUARDIAN_EVENT_1 } from "./expedition-guardians-event";
+import { RUNE_SPECIAL_AGILITY_2, RUNE_SPECIAL_ATTACK_2, RUNE_SPECIAL_DEFENSE_2, RUNE_SPECIAL_WISDOM_2 } from "src/api/items/item/data/runes";
+
+export const EXPEDITION_EVENT_ANNIVERSARY: ExpeditionDto = {
+    uid: 'expedition-e0',
+    name: 'Forest of wishes',
+    hint: `Step into the Forest of Wishes, where magic awaits! Explore this enchanted woodland and uncover valuable ingredients and equipment for dragons to collect.`,
+    level: 10,
+    gold: 100,
+    extraGold: 100,
+    loots: [
+        SHARD_BLANK, SHARD_BLANK, SHARD_BLANK, SHARD_BLANK, SHARD_BLANK, SHARD_BLANK, SHARD_BLANK, 
+        SHARD_ATTACK, SHARD_ATTACK, SHARD_ATTACK, SHARD_ATTACK,
+        SHARD_DEFENCE, SHARD_DEFENCE, SHARD_DEFENCE, SHARD_DEFENCE,
+        SHARD_AGILITY, SHARD_AGILITY, SHARD_AGILITY, SHARD_AGILITY,
+        SHARD_WISDOM, SHARD_WISDOM, SHARD_WISDOM, SHARD_WISDOM,
+        SHARD_UNITY, SHARD_UNITY,
+    ],
+    extraLoots: [
+        RUNE_SPECIAL_AGILITY_2,
+        RUNE_SPECIAL_ATTACK_2,
+        RUNE_SPECIAL_DEFENSE_2,
+        RUNE_SPECIAL_WISDOM_2,
+    ],
+    minimumActionTime: EXPEDITION_TIME_SCALE * 6 * 60 * 60 * 1000,
+    guardian: GUARDIAN_ANNIVERSARY,
+}
 
 export const EXPEDITION_EVENT_1: ExpeditionDto = {
     uid: 'expedition-e1',
@@ -28,5 +54,6 @@ export const EXPEDITION_EVENT_1: ExpeditionDto = {
 };
 
 export const EXPEDITIONS_EVENT: ExpeditionDto[] = [
+    EXPEDITION_EVENT_ANNIVERSARY,
     // EXPEDITION_EVENT_1,
 ];
