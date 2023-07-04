4ddf5e9d22d4dddee2abc1c08a504b967d70f433
matfij <mateusz.michal.fijak@gmail.com>
2023-07-04 21:57:26 +0200

    refactor project structure

--- generator/game-data/run.py
+++
@@ -1,9 +0,0 @@
-from src.dragon_service import DragonService
-
-# .venv\scripts\activate
-
-MIN_LEVEL = 200
-MAX_LEVEL = 300
-
-for ind in range(40):
-    DragonService.create_dragon(MIN_LEVEL, MAX_LEVEL)
