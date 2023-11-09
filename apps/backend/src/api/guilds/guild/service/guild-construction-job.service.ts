5f5108cd5b4d9979061cebc3ec3674756c767a48
matfij <mateusz.michal.fijak@gmail.com>
2023-11-09 21:57:40 +0100

    add guild resource limit

--- apps/backend/src/api/guilds/guild/service/guild-construction-job.service.ts
+++ apps/backend/src/api/guilds/guild/service/guild-construction-job.service.ts
@@ -4,19 +4,22 @@ import { Guild } from '../model/guild.entity';
 import { Repository } from 'typeorm';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { FORGE_UPGRADES, QUARRY_UPGRADES, SAWMILL_UPGRADES } from '../data/structure-upgrades';
-import { GUILD_RESOURCES_GAIN } from '../../../../configuration/backend.config';
+import { GUILD_RESOURCES_GAIN, GUILD_RESOURCE_LIMIT } from '../../../../configuration/backend.config';
 
 @Injectable()
 export class GuildConstructionJobService {
     constructor(@InjectRepository(Guild) private guildRepository: Repository<Guild>) {}
 
-    @Cron(CronExpression.EVERY_HOUR)
+    @Cron(CronExpression.EVERY_30_SECONDS)
     async updateResources() {
         const guilds = await this.guildRepository.find();
         for (const guild of guilds) {
             guild.wood += Math.round(GUILD_RESOURCES_GAIN * SAWMILL_UPGRADES[guild.sawmillLevel].utility);
+            guild.wood = Math.min(guild.wood, GUILD_RESOURCE_LIMIT);
             guild.stone += Math.round(GUILD_RESOURCES_GAIN * QUARRY_UPGRADES[guild.quarryLevel].utility);
+            guild.stone = Math.min(guild.stone, GUILD_RESOURCE_LIMIT);
             guild.steel += Math.round(GUILD_RESOURCES_GAIN * FORGE_UPGRADES[guild.forgeLevel].utility);
+            guild.steel = Math.min(guild.steel, GUILD_RESOURCE_LIMIT);
         }
         await this.guildRepository.save(guilds);
     }
